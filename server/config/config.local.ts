2fd1e7f8875216deeaaf9b5497847416d2925dd5
xiaotian <952822399@qq.com>
2019-12-29 15:43:53 +0800

    跨域配置

--- server/config/config.local.ts
+++ server/config/config.local.ts
@@ -2,16 +2,5 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  config.security = {
-    csrf: {
-      enable: false,
-      ignoreJSON: true
-    },
-    domainWhiteList: ['http://127.0.0.1:8080']
-  };
-  config.cors = {
-    origin:'*',
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-  };
   return config;
 };
--- server/config/config.prod.ts
+++ server/config/config.prod.ts
@@ -2,16 +2,5 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  config.security = {
-    csrf: {
-      enable: false,
-      ignoreJSON: true
-    },
-    domainWhiteList: ['http://123.207.60.132']
-  };
-  config.cors = {
-    origin:'*',
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-  };
   return config;
 };
