650bd046685e62ada838c874932a0343afdc184d
xiaotian <952822399@qq.com>
2019-12-29 16:29:35 +0800

    跨域调试

--- server/config/config.local.ts
+++ server/config/config.local.ts
@@ -2,5 +2,18 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+  config.security = {
+    csrf: {
+      enable: false, // 前后端分离，post请求不方便携带_csrf
+      ignoreJSON: true
+    },
+    domainWhiteList: ['192.168.1.6', '123.207.60.132'], //配置白名单
+  };
+
+  config.cors = {
+    // origin: '*', //允许所有跨域访问，注释掉则允许上面 白名单 访问
+    credentials: true, // 允许跨域请求携带cookies
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  }
   return config;
 };
