091d180a0071e6018cdf7686c59f50c39b700542
xiaotiandada <952822399@qq.com>
2020-01-03 01:42:55 +0800

    update nav

--- client/src/views/layout.vue
+++ client/src/views/layout.vue
@@ -14,7 +14,7 @@
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>导航一</span>
+            <span>Matataki</span>
           </template>
           <el-menu-item-group>
             <template slot="title">分组一</template>
