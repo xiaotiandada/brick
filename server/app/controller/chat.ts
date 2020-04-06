d11b0755ac6f43f1218db89d040f351d61b0e067
xiaotian <952822399@qq.com>
2020-04-06 22:16:45 +0800

    奇奇怪怪

--- server/app/controller/chat.ts
+++ server/app/controller/chat.ts
@@ -4,8 +4,8 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
     console.log('chat :', process.pid);
-    console.log(ctx.args)
-    let { token, firstId, lastId } = ctx.args[0]
+    console.log(ctx.args);
+    const { token, firstId, lastId } = ctx.args[0];
     for (let index = firstId; index <= lastId; index++) {
       const res = await ctx.service.user.say(index, token);
       ctx.socket.emit('res', res);
--- server/app/controller/m.ts
+++ server/app/controller/m.ts
@@ -6,11 +6,13 @@ export default class MController extends Controller {
     const { url, token } = ctx.request.body;
     ctx.body = await ctx.service.m.postImport(url, token);
   }
+
   public async postPublish() {
     const { ctx } = this;
     const { title, cover, content, token } = ctx.request.body;
     ctx.body = await ctx.service.m.postPublish(title, cover, content, token);
   }
+
   public async postLikes() {
     const { ctx } = this;
     const { firstId, lastId, token } = ctx.request.body;
