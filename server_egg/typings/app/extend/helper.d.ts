762e0fce36ca7dffe7bc400bee224835c1e4b840
xiaotian <952822399@qq.com>
2020-08-02 00:29:56 +0800

    elasticsearch

--- server_egg/typings/app/extend/helper.d.ts
+++ server_egg/typings/app/extend/helper.d.ts
@@ -1,8 +1,8 @@
-// This file is created by egg-ts-helper@1.25.6
+// This file is created by egg-ts-helper@1.25.7
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExtendIHelper from '../../../app/extend/helper';
+import ExtendIHelper = require('../../../app/extend/helper');
 type ExtendIHelperType = typeof ExtendIHelper;
 declare module 'egg' {
   interface IHelper extends ExtendIHelperType { }
--- server_egg/typings/app/service/index.d.ts
+++ server_egg/typings/app/service/index.d.ts
@@ -1,7 +1,11 @@
-// This file is created by egg-ts-helper@1.25.6
+// This file is created by egg-ts-helper@1.25.7
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+type AnyClass = new (...args: any[]) => any;
+type AnyFunc<T = any> = (...args: any[]) => T;
+type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
+type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
 import ExportTest from '../../../app/service/Test';
 import ExportAccount from '../../../app/service/account';
 import ExportCrawler from '../../../app/service/crawler';
@@ -14,14 +18,14 @@ import ExportWx from '../../../app/service/wx';
 
 declare module 'egg' {
   interface IService {
-    test: ExportTest;
-    account: ExportAccount;
-    crawler: ExportCrawler;
-    jmeter: ExportJmeter;
-    m: ExportM;
-    notification: ExportNotification;
-    transation: ExportTransation;
-    user: ExportUser;
-    wx: ExportWx;
+    test: AutoInstanceType<typeof ExportTest>;
+    account: AutoInstanceType<typeof ExportAccount>;
+    crawler: AutoInstanceType<typeof ExportCrawler>;
+    jmeter: AutoInstanceType<typeof ExportJmeter>;
+    m: AutoInstanceType<typeof ExportM>;
+    notification: AutoInstanceType<typeof ExportNotification>;
+    transation: AutoInstanceType<typeof ExportTransation>;
+    user: AutoInstanceType<typeof ExportUser>;
+    wx: AutoInstanceType<typeof ExportWx>;
   }
 }
